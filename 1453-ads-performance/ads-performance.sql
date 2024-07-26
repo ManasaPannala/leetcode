b1a06efc3cad3788e42f0a9504e3e7f67c3ff38f
ManasaPannala <84516933+ManasaPannala@users.noreply.github.com>
2024-07-25 17:36:10 -0700

    Time: 520 ms (65.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1453-ads-performance/ads-performance.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+
+select ad_id, ifnull(round(avg(case when action='Clicked' then 1 
+when action='Viewed' then 0 else null end)*100,2),0) ctr
+from Ads 
+group by ad_id
+order by ctr desc, ad_id 
+
+
