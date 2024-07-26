55d4f7cb299483f172912678c6b955a16b5b7a36
ManasaPannala <84516933+ManasaPannala@users.noreply.github.com>
2024-07-25 17:05:42 -0700

    Time: 746 ms (28.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1425-weather-type-in-each-country/weather-type-in-each-country.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+select c.country_name, case when avg(w.weather_state)<=15 then 'Cold'
+when avg(w.weather_state)>=25 then 'Hot' else 'Warm' end weather_type
+from Countries c join Weather w on c.country_id = w.country_id
+where w.day like '2019-11-%%'
+group by c.country_id
